c28d1982887a11e9529863d7a70a35b4c67dce44
Adminsvu <Baikonur96@yandex.ru>
2022-12-29 16:23:36 +0300

    ver 3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,103 @@
 
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure("hibernate.cfg.xml").build();
+        Metadata metadata = new MetadataSources(registry).getMetadataBuilder().build();
+        SessionFactory sessionFactory = metadata.getSessionFactoryBuilder().build();
 
-        String url = "jdbc:mysql://localhost:3306/skillbox";
+        Session session = sessionFactory.openSession();
+        String hql = "SELECT course_name FROM PurchaseList WHERE course_name LIKE 'PHP-разработчик с 0 до PRO'";
+        List<Course> courseList = session.createQuery(hql).getResultList();
+        System.out.println(courseList.size());
+
+
+//        String hql = "From " + Course.class.getSimpleName() + " Where price > 120000";
+//        List<Course> courseList = session.createQuery(hql).getResultList();
+//
+//        System.out.println(courseList.size());
+
+//        CriteriaBuilder builder = session.getCriteriaBuilder();
+//        CriteriaQuery<Course> query = builder.createQuery(Course.class);
+//        Root<Course> root = query.from(Course.class);
+//        query.select(root);
+//        List<Course> courseList = session.createQuery(query).getResultList();
+//
+//        for (Course course : courseList) {
+//            System.out.println(course.getName());
+//        }
+
+
+//        Transaction transaction = session.beginTransaction();
+//        Course course = session.get(Course.class, 1);
+//        List<Student> studentList = course.getStudents();
+//        studentList.forEach(student -> {
+//            System.out.println(student.getName());
+//        });
+//        transaction.commit();
+        sessionFactory.close();
+        }
+}
+
+
+// TODO: 20.12.2022 WORK_16.5
+/*    StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+            .configure("hibernate.cfg.xml").build();
+    Metadata metadata = new MetadataSources(registry).getMetadataBuilder().build();
+    SessionFactory sessionFactory = metadata.getSessionFactoryBuilder().build();
+    Session session = sessionFactory.openSession();
+    Transaction transaction = session.beginTransaction();
+
+    Course course = session.get(Course.class, 45);
+        course.setName("Управление продуктом");
+
+
+// TODO: 20.12.2022 Создаем новый курс
+//        Course course = new Course();
+//        course.setName("Новый курс");
+//        course.setType(CourseType.BUSINESS);
+//        course.setTeacherId(1);
+
+                // TODO: 20.12.2022 удаляем курс        
+//        session.delete(course);
+
+
+                session.save(course);
+                transaction.commit();
+
+
+
+//        Course course = session.get(Course.class, 1);
+//        System.out.println(course.getName());
+                sessionFactory.close();*/
+
+
+
+
+
+
+
+
+// TODO: 20.12.2022 Старый метод парсинга таблицы
+/*        String url = "jdbc:mysql://localhost:3306/skillbox";
         String user = "root";
         String pass = "jcnhjdtw22";
 
@@ -22,6 +111,10 @@ public class Main {
 
             ResultSet resultSet = statement.executeQuery("SELECT * FROM Courses");
             while(resultSet.next()){
+                Course course = new Course();
+                course.setId(resultSet.getInt("id"));
+                course.setName(resultSet.getString("name"));
+
                 String courseName = resultSet.getString("name");
                 System.out.println(courseName);
             }
@@ -32,6 +125,4 @@ public class Main {
 
         }catch (Exception ex){
             ex.printStackTrace();
-        }
-        }
-}
+        }*/
